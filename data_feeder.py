43c291d30082212c5850724fb660e53c26fdf7b7
paulko2003 <payloskollias@gmail.com>
2022-07-17 20:37:43 +0300

    made data reader work but be slow

--- data_feeder.py
+++ data_feeder.py
@@ -1,7 +1,7 @@
 #this script it made as an api between how we get the data and how its being fed to the code
 #whenever the end function is called there needs to be returned a dict in the form of (afm: "my afm", email: "my email", pdf: "my email")
 #currently we are getting the data by a file containing afm : email, then we search for the pdf with that afm.
-
+import glob as gl
 import os
 import fnmatch as fn
 
@@ -13,42 +13,31 @@ class get_data_test_form:
     
     #gets the data from each line and saves it to my_dict
     def strip_data(self, line: str): 
-        email=''
-        afm=''
         passed=False
         line=line.replace(' ','')
         line=line.replace('\n','')
-        for i in line:
-            if i==':':
-                passed=True
-            elif not passed:
-                afm+=i
-            else:
-                email+=i
-        self.my_dict['afm']=afm
-        self.my_dict['emai']=email
+        data=line.split(':') 
+        self.my_dict['afm']=data[0]
+        self.my_dict['emai']=data[1]
 
         
 
-    #searches and returns file path if exists if not returns empty string ''
+    #searches and saves file path in my_dict if exists and returns True/False acordigly
     def pdf_file_search(self, folder: str, afm: str):
-        path=''
-        for filename in os.listdir(folder):
-            file_path = os.path.join(folder, filename)
-            try:
-                if fn.fnmatch(file_path,'*'+afm+'*'):
-                    path=file_path
-            except Exception as e:
-                print('Failed to delete %s. Reason: %s' % (file_path, e))
-        return path
+        exists= False
+        direcotry = gl.glob(folder+"/*"+afm+"*")
+        if len(direcotry)==1:
+            exists=True
+            self.my_dict["pdf"]= direcotry[0]
+        return exists
 
     def _test(self,file_path= 'Testing/File_test/Test_file_1.txt',file_pdfs='Testing/PDF_files/pdf_files_1'):
         with open(file_path) as file:
             for index,line in enumerate(file):
                 self.strip_data(line)
-                print(index)
                 if not self.pdf_file_search(file_pdfs,self.my_dict["afm"]):
                     print(f"issue in {  line  }")
+                
                 
 
                 
